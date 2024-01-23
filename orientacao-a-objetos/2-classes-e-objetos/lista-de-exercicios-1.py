8aee4c3daf6b55f953ce27778eee442aa4f4c41b
Gabriela A. Z. Nunez <108434852+gabiapp@users.noreply.github.com>
2024-01-23 15:22:00 -0300

    Update lista-de-exercicios-1.py
    Adicionada resolução da lista de exercícios um utilizando classes

--- orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
+++ orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
@@ -2,12 +2,46 @@
 # Um carro tem os seguintes atributos: ligado, cor, modelo, velocidade.
 # Um carro tem os seguintes comportamentos: liga, desliga, acelera, desacelera.
 
+class Carro:
+    def __init__(self):
+      self.ligado = False
+      self.cor = "Vermelho"
+      self.modelo = 2023
+      self.velocidade = 0
 
+    def ligar(self):
+      self.ligado = True
 
+    def desligar(self):
+      self.ligado = False
+
+    def acelerar(self):
+      self.velocidade += 10
+
+    def desacelerar(self):
+      self.velocidade -= 10
+      
 # Crie uma instância da classe carro.
+carro_alison = Carro()
 
+print("Você está em seu carro, ele está ligado, clique em 1 para acelerar ou em 0 para desacelerar")
+while True:
+    # Faça o carro "andar" utilizando os métodos da sua classe.
+    acelerar = int(input("Acelere ou freie:\n"))
 
-# Faça o carro "andar" utilizando os métodos da sua classe.
+    if acelerar == 1:
+        carro_alison.acelerar()
+        print(f"Você está dirigindo a {carro_alison.velocidade}km por hora")
+    # Faça o carro "parar" utilizando os métodos da sua classe.
+    elif acelerar == 0:
+        carro_alison.desacelerar()
+        print(f"Você está freando e sua velocidade é de {carro_alison.velocidade}km por hora")
+    else: 
+        print("Digite 0 ou 1 para que sua escolha seja devidamente interpretada")
 
-
-# Faça o carro "parar" utilizando os métodos da sua classe.
+    if carro_alison.velocidade == 0:
+       print("Você está parado")
+       break
+print("Seu carro desligou")
+    
+    
